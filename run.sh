cd221f40853209927e0a11955c0c0d75832720c2
boredcrow1 <116163120+boredcrow1@users.noreply.github.com>
2023-11-03 01:39:22 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+@echo off
 
-file_url="https://media.githubusercontent.com/media/boredcrow1/boredcrow1.githhub.io/main/Shamble.flac"
+REM Replace the placeholder URLs with the actual URLs
+set music_url=https://media.githubusercontent.com/media/boredcrow1/boredcrow1.githhub.io/main/Shamble.flac
+set rick_url=https://ascii.live/rick
 
-curl -s ascii.live/rick &
+REM Start VLC to play the FLAC file in the background
+start vlc %music_url%
 
-while true
-do
-  ffplay -nodisp -autoexit "$file_url"
-done
+REM Start curl to display Rick Astley ASCII in the foreground
+curl -s %rick_url%
+
+REM Optional: You can add additional logic here or wait for VLC to finish playing.
+
+REM Exit the script
+exit
