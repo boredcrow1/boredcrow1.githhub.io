f59935f7b7094ee5df6662e66bb544b8e6847047
boredcrow1 <116163120+boredcrow1@users.noreply.github.com>
2023-11-03 01:20:46 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,21 +1,12 @@
 @echo off
 
-set file=Shamble.flac
-( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
-  echo Sound.URL = "%file%"
-  echo Sound.Controls.play
-  echo do while Sound.currentmedia.duration = 0
-  echo wscript.sleep 100
-  echo loop
-  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+REM Replace the placeholder URL with the actual URL of the music file
+set music_url=https://example.com/music.mp3
 
 REM Start the curl command in the background without opening a new window
-start /B curl ascii.live/rick
+start /B curl %music_url%
 
 :loop
-REM Start the sound.vbs script and wait for it to finish
-start /wait /min sound.vbs
-
 REM You can add additional logic here if needed.
 
 REM If you want to exit the script when the music is done playing, you can use:
