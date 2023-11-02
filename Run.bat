ce5d097af0c9e0198f2cabcb7969e7412de0d8fc
boredcrow1 <116163120+boredcrow1@users.noreply.github.com>
2023-11-03 02:41:25 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -3,11 +3,11 @@
 REM
 set music_url=https://media.githubusercontent.com/media/boredcrow1/boredcrow1.githhub.io/main/Shamble.flac
 
-REM Start the curl command in the background without opening a new window
+REM
 start /B curl %music_url%
 
 :loop
-REM You can add additional logic here if needed.
+REM
 
 REM If you want to exit the script when the music is done playing, you can use:
 REM exit
