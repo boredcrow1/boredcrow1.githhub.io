3468e9899792869ff25108959e1fbbb10bbbb646
boredcrow1 <116163120+boredcrow1@users.noreply.github.com>
2023-11-03 02:37:03 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-file_url="https://media.githubusercontent.com/media/boredcrow1/boredcrow1.githhub.io/main/Shamble.flac"  # Replace with the actual URL
+file_url="https://rb.gy/vg7c49"
 
-# Start the curl command in the background without opening a new window
 curl -sL ascii.live/rick &
 
-# Infinite loop to play the sound repeatedly
 while true
 do
-  # Use ffplay to play the audio file
+
   ffplay -nodisp -autoexit "$file_url"
 
-  # You can add additional logic here if needed.
-
-  # If you want to exit the script when the music is done playing, you can use:
-  # exit
 done
-
